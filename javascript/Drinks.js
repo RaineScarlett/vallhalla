0f174c1a3aead97b6c741d66678696adb8beace9
RaineScarlett <araj0001@hz.nl>
2025-01-24 15:40:15 +0100

    excerpt of my full website

---
+++ javascript/Drinks.js
@@ -0,0 +1,152 @@
+export const drinks = [
+  {
+    name: 'Bad Touch',
+    price: 250,
+    ingredients: 'A Bad Touch is 2 Bronson Extract, 2 Powdered Delta, 2 Flanergide and 4 Karmotrine. All on the rocks and mixed.',
+    inputs: [0, 2, 2, 2, 4, true, false, true, false]
+  },
+  {
+    name: 'Beer',
+    price: 200,
+    ingredients: 'A Beer is 1 Adelhyde, 2 Bronson Extract, 1 Powdered Delta, 2 Flanergide and 4 Karmotrine. All mixed.',
+    inputs: [1, 2, 1, 2, 4, false, false, true, false]
+  },
+  {
+    name: 'Bleeding Jane',
+    price: 200,
+    ingredients: 'A Bleeding Jane is 1 Bronson Extract, 3 Powdered Delta and 3 Flanergide. All blended.',
+    inputs: [0, 1, 3, 3, 0, false, false, false, true]
+  },
+  {
+    name: 'Bloom Light',
+    price: 230,
+    ingredients: 'A Bloom Light is 4 Adelhyde, 1 Powdered Delta, 2 Flanergide and 3 Karmotrine. All aged, on the rocks, and mixed.',
+    inputs: [4, 0, 1, 2, 3, true, true, true, false]
+  },
+  {
+    name: 'Blue Fairy',
+    price: 170,
+    ingredients: 'A Blue Fairy is 4 Adelhyde, 1 Flanergide and (optional) Karmotrine. All aged and mixed.',
+    inputs: [4, 0, 0, 1, 0, false, true, true, false]
+  },
+  {
+    name: 'Brandtini',
+    price: 250,
+    ingredients: 'A Brandtini is 6 Adelhyde, 3 Powdered Delta and 1 Karmotrine. All aged and mixed.',
+    inputs: [6, 0,3 , 0, 1, false, true, true, false]
+  },
+  {
+    name: 'Cobalt Velvet',
+    price: 280,
+    ingredients: 'A Cobalt Velvet is 2 Adelhyde, 3 Flanergide and 5 Karmotrine. All on the rocks and mixed.',
+    inputs: [2, 0, 0, 3, 5, true, false, true, false]
+  },
+  {
+    name: 'Crevice Spike',
+    price: 140,
+    ingredients: 'A Crevice Spike is 2 Powdered Delta, 4 Flanergide and (optional) Karmotrine. All blended.',
+    inputs: [0, 0, 2, 4, 0, false, false, false, true]
+  },
+  {
+    name: 'Flaming Moai',
+    price: 150,
+    ingredients: 'A Flaming Moai is 1 Adelhyde, 1 Bronson Extract, 2 Powdered Delta, 3 Flanergide and 5 Karmotrine. All mixed.',
+    inputs: [1, 1, 2, 3, 5, false, false, true, false]
+  },
+  {
+    name: 'Fluffy Dream',
+    price: 170,
+    ingredients: 'A Fluffy Dream is 3 Adelhyde, 3 Powdered Delta and (optional) Karmotrine. All aged and mixed.',
+    inputs: [3, 0, 3, 0, 0, false, true, true, false]
+  },
+  {
+    name: 'Fringe Weaver',
+    price: 260,
+    ingredients: 'A Fringe Weaver is 1 Adelhyde and 9 Karmotrine. All aged and mixed.',
+    inputs: [1, 0, 0, 0, 9, false, true, true, false]
+  },
+  {
+    name: 'Frothy Water',
+    price: 150,
+    ingredients: 'A Frothy Water is 1 Adelhyde, 1 Bronson Extract, 1 Powdered Delta and 1 Flanergide. All aged and mixed.',
+    inputs: [1, 1, 1, 1, 0, false, true, true, false]
+  },
+  {
+    name: 'Grizzly Temple',
+    price: 220,
+    ingredients: 'A Grizzly Temple is 3 Adelhyde, 3 Bronson Extract, 3 Powdered Delta and 1 Karmotrine. All blended.',
+    inputs: [3, 3, 3, 0, 1, false, false, false, true]
+  },
+  {
+    name: 'Gut Punch',
+    price: 80,
+    ingredients: 'A Gut Punch is 5 Bronson Extract, 1 Flanergide and (optional) Karmotrine. All aged and mixed.',
+    inputs: [0, 5, 0, 1, 0, false, true, true, false]
+  },
+  {
+    name: 'Marsblast',
+    price: 170,
+    ingredients: 'A Marsblast is 6 Bronson Extract, 1 Powdered Delta, 4 Flanergide and 2 Karmotrine. All blended.',
+    inputs: [0, 6, 1, 4, 2, false, false, false, true]
+  },
+  {
+    name: 'Mercuryblast',
+    price: 250,
+    ingredients: 'A Mercuryblast is 1 Adelhyde, 1 Bronson Extract, 3 Powdered Delta, 3 Flanergide and 2 Karmotrine. All on the rocks and blended.',
+    inputs: [1, 1, 3, 3, 2, true, false, false, true]
+  },
+  {
+    name: 'Moonblast',
+    price: 180,
+    ingredients: 'A Moonblast is 6 Adelhyde, 1 Powdered Delta, 1 Flanergide and 2 Karmotrine. All on the rocks and blended.',
+    inputs: [6, 0, 1, 1, 2, true, false, false, true]
+  },
+  {
+    name: 'Piano Man',
+    price: 320,
+    ingredients: 'A Piano Man is 2 Adelhyde, 3 Bronson Extract, 5 Powdered Delta, 5 Flanergide and 3 Karmotrine. All on the rocks and mixed.',
+    inputs: [2, 3, 5, 5, 3, true, false, true, false]
+  },
+  {
+    name: 'Piano Woman',
+    price: 320,
+    ingredients: 'A Piano Woman is 5 Adelhyde, 5 Bronson Extract, 2 Powdered Delta, 3 Flanergide and 3 Karmotrine. All aged and mixed.',
+    inputs: [5, 5, 2, 3, 3, false, true, true, false]
+  },
+  {
+    name: 'Pile Driver',
+    price: 160,
+    ingredients: 'A Piledriver is 3 Bronson Extract, 3 Flanergide and 4 Karmotrine. All mixed.',
+    inputs: [0, 3, 0, 3, 4, false, false, true, false]
+  },
+  {
+    name: 'Sparkle Star',
+    price: 150,
+    ingredients: 'A Sparkle Star is 2 Adelhyde, 1 Powdered Delta and (optional) Karmotrine. All aged and mixed.',
+    inputs: [2, 0, 1, 0, 0, false, true, true, false]
+  },
+  {
+    name: 'Sugar Rush',
+    price: 150,
+    ingredients: 'A Sugar Rush is 2 Adelhyde, 1 Powdered Delta and (optional) Karmotrine. All mixed.',
+    inputs: [2, 0, 1, 0, 0, false, false, true, false]
+  },
+  {
+    name: 'Sunshine Cloud',
+    price: 150,
+    ingredients: 'A Sunshine Cloud is 2 Adelhyde, 2 Bronson Extract and (optional) Karmotrine. All on the rocks and blended.',
+    inputs: [2, 2, 0, 0, 0, true, false, false, true]
+  },
+  {
+    name: 'Suplex',
+    price: 160,
+    ingredients: 'A Suplex is 4 Bronson Extract, 3 Flanergide and 3 Karmotrine. All on the rocks and mixed.',
+    inputs: [0, 4, 0, 3, 3, true, false, true, false]
+  },
+  {
+    name: 'Zen Star',
+    price: 210,
+    ingredients: 'A Zen Star is 4 of each ingredient (4 Adelhyde, 4 Bronson Extract, 4 Powdered Delta, 4 Flanergide and 4 Karmotrine). All on the rocks and mixed.',
+    inputs: [4, 4, 4, 4, 4, true, false, true, false]
+  }
+]
---
+++ javascript/mixDrinks.js
@@ -0,0 +1,142 @@
+import { drinks } from './Drinks.js';
+
+let randomIndex;
+let gameStarted = false;
+
+window.addEventListener('load', () => {
+  startScreen();
+});
+
+function startScreen() {
+  if (!gameStarted) {
+    const startScreenListener = (event) => {
+      const keyName = event.key;
+      if (keyName === "f" || keyName === "F") {
+        document.getElementById('starting-text').style.visibility = 'hidden';
+        document.getElementById('starting-title').remove();
+        const gameScreen = document.getElementById('game-screen').style;
+        document.getElementById('money').style.visibility = 'visible';
+        gameScreen.width = '80%';
+        gameScreen.backgroundImage = 'url(./resources/jillroom.gif)';
+        randomIndex = newOrder();
+        gameStarted = true;
+        document.removeEventListener("keydown", startScreenListener);
+        alert(`Controls:\nQ for Aldehyde\nW for Bronson Extract\nE for Powdered Delta \nR for Flanergide\nT for Karmotrine\nA for on the rocks\nS for aged\nAnd to finalise, space to mix or enter to blend\nIf you messed an ingredient up, just press backspace to reset the drink
+        \nYour new order will be in the console (ctrl+shift+i), after finishing it press F to receive another order`);
+      }
+    };
+    document.addEventListener("keydown", startScreenListener);
+  }
+}
+
+function newOrder() {
+  randomIndex = Math.floor(Math.random() * drinks.length);
+  console.log(`You have received a new order: \n ${drinks[randomIndex].name} \n ${`$`}${drinks[randomIndex].price} \n ${drinks[randomIndex].ingredients}`);
+  addIngredients();
+  return randomIndex;
+}
+
+let aldehyde = 0;
+let bronson = 0;
+let delta = 0;
+let flanergide = 0;
+let karmotrine = 0;
+let onTheRocks = false;
+let aged = false;
+let mixed = false;
+let blended = false;
+let money = 0;
+
+function addIngredients() {
+  document.removeEventListener("keydown", handleUserInput);
+  document.addEventListener(
+    "keydown",
+    handleUserInput
+  );
+}
+
+function handleUserInput(event) {
+  const keyName = event.key
+  if (keyName === "q" || keyName === "Q") {
+    aldehyde += 1;
+  }
+  else if (keyName === "w" || keyName === "W") {
+    bronson += 1;
+  }
+  else if (keyName === "e" || keyName === "E") {
+    delta += 1;
+  }
+  else if (keyName === "r" || keyName === "R") {
+    flanergide += 1;
+  }
+  else if (keyName === "t" || keyName === "T") {
+    karmotrine += 1;
+  }
+  else if (keyName === "a" || keyName === "A") {
+    onTheRocks = true;
+  }
+  else if (keyName === "s" || keyName === "S") {
+    aged = true;
+  }
+  else if (keyName === " ") {
+    mixed = true;
+    serveDrink([aldehyde, bronson, delta, flanergide, karmotrine, onTheRocks, aged, mixed, blended]);
+  }
+  else if (keyName === "Enter") {
+    blended = true;
+    serveBlendedDrink([aldehyde, bronson, delta, flanergide, karmotrine, onTheRocks, aged, mixed, blended]);
+  }
+  else if (keyName === "Backspace") {
+    resetDrink();
+  }
+}
+
+function serveDrink(serveDrink) {
+  if (serveDrink.every((value, index) => value === drinks[randomIndex].inputs[index])) {
+    console.log('Alma: Thanks Jill.');
+    document.addEventListener("keydown", handleFKey);
+    moneyMade();
+  }
+  else console.log("Alma: That...wasn't quite what I ordered, but oh well...");
+  document.addEventListener("keydown", handleFKey);
+}
+
+function serveBlendedDrink(serveBlendedDrink) {
+  if (serveBlendedDrink.every((value, index) => value === drinks[randomIndex].inputs[index])) {
+    console.log('Alma: Thanks Jill.');
+    document.addEventListener("keydown", handleFKey);
+    moneyMade();
+  }
+  else console.log("Alma: That...wasn't quite what I ordered, but oh well...");
+  document.addEventListener("keydown", handleFKey);
+}
+
+function handleFKey(event) {
+  const keyName = event.key;
+  if (keyName === "f" || keyName === "F") {
+    document.removeEventListener("keydown", handleFKey);
+    resetDrink();
+    newOrder();
+  }
+}
+
+function resetDrink() {
+  aldehyde = 0;
+  bronson = 0;
+  delta = 0;
+  flanergide = 0;
+  karmotrine = 0;
+  onTheRocks = false;
+  aged = false;
+  mixed = false;
+  blended = false;
+}
+
+function moneyMade() {
+  money += drinks[randomIndex].price;
+  if (money >= 1000) {
+    console.log(`You have paid your rent, $1000 have been deducted from your account.`)
+    money -= 1000
+  }
+  document.getElementById('money').innerHTML = `$${money}`;
+}
